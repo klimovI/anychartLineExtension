75c9ffcd42a3e86fcce7613317aa542c37d33378
KlimovI <irracli@gmail.com>
2019-12-25 17:01:33 +0800

    moved chart creation to controller

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,7 @@
-export default ['$scope', $scope => {
+export default ['$scope', '$element', ($scope, $element) => {
   console.log('controller');
+  // chart container element
+  const chartContainer = $element[0].querySelector('#chartContainer');
   const {
     qMatrix
   } = $scope.layout.qHyperCube.qDataPages[0];
@@ -11,6 +13,9 @@ export default ['$scope', $scope => {
   });
   // create a chart
   $scope.layout.anychartLine = window.anychart.line();
+  const chart = $scope.layout.anychartLine;
   // set the data
-  $scope.layout.anychartLine.line(data);
+  chart.line(data);
+  // set the container for chart
+  chart.container(chartContainer);
 }];
--- src/paint.js
+++ src/paint.js
@@ -1,10 +1,7 @@
 export default ($element, layout) => {
   console.log('paint');
   const chart = layout.anychartLine;
-  const chartContainer = $element[0].querySelector('#chartContainer');
 
-  // set the container
-  chart.container(chartContainer);
   // initiate drawing the chart
   chart.draw();
 };
